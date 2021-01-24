8a72498100e95d610c4fcb450ac5e62f8497ea29
hankhank10 <m.a.hankinson@gmail.com>
2021-01-24 19:56:30 +0000

    New gui

--- client_gui.py
+++ client_gui.py
@@ -89,7 +89,7 @@ def update_location():
 window = tk.Tk()
 window.title("Find My Plane")
 window.resizable(False, False)
-window.geometry("420x250")
+window.geometry("400x270")
 
 # Create labels
 server_status_label = tk.Label(
